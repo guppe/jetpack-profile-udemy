ee7ab44bda75cd654578206b97e05758d449586f
guppe <mst39639toi8@gmail.com>
2023-02-08 01:47:06 +0900

    プロフィール画像を追加する

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetprofile.ui.theme.JetProfileTheme
@@ -31,7 +33,11 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally,
                         modifier = Modifier.padding(20.dp)
                     ) {
-
+                        // プロフィール画像
+                        Image(
+                            painter = painterResource(id = R.drawable.img_profile),
+                            contentDescription = "プロフィール"
+                        )
                     }
                 }
             }

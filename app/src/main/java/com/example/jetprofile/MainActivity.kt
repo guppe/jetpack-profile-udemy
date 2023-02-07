57521bc1068739c9ad4a94dab16a4bac5cc970dd
guppe <mst39639toi8@gmail.com>
2023-02-08 01:54:18 +0900

    プロフィールに画像のサイズを修正する

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -35,7 +37,10 @@ class MainActivity : ComponentActivity() {
                         // プロフィール画像
                         Image(
                             painter = painterResource(id = R.drawable.img_profile),
-                            contentDescription = "プロフィール"
+                            contentDescription = "プロフィール",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp))
                         )
                         Spacer(modifier = Modifier.height(20.dp))
 
@@ -46,7 +51,8 @@ class MainActivity : ComponentActivity() {
                             fontSize = 14.sp,
                             fontWeight = FontWeight.ExtraBold,
                         )
-
+                        Spacer(modifier = Modifier.height(20.dp))
+                        
                         // 職業
                         Text(
                             text = "Profession: Student",
